80337962096323ffb01de98ebab3c72c22f5ef6b
Pro <gigitoto08@gmail.com>
2026-03-20 05:12:39 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-// /src/App.tsx
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 
-// صفحات المشروع
 import Index from "./pages/Index";
-import VideoPage from "./pages/video"; // مطابق لاسم الملف video.tsx
+import VideoPage from "./pages/video";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -20,13 +18,8 @@ const App = () => {
         <Sonner />
         <BrowserRouter>
           <Routes>
-            {/* الصفحة الرئيسية */}
             <Route path="/" element={<Index />} />
-
-            {/* صفحة الفيديو */}
             <Route path="/video/:id" element={<VideoPage />} />
-
-            {/* صفحة 404 */}
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
