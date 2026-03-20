fa49c09864b6f933a34b8fcd8bb781bacde0b4c7
Pro <gigitoto08@gmail.com>
2026-03-20 03:07:42 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// src/App.tsx
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
@@ -6,21 +7,24 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import VideoPage from "./pages/video"; // 🔥 مهم جدًا
+import VideoPage from "./pages/video"; // 🔥 يتوافق مع اسم الملف video.tsx
 
+// إنشاء Query Client
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Toaster />
-      <Sonner />
+      {/* إشعارات */}
+      <Toaster /> {/* Custom Toaster */}
+      <Sonner />  {/* Sonner Toaster */}
+
       <BrowserRouter>
         <Routes>
           {/* الصفحة الرئيسية */}
           <Route path="/" element={<Index />} />
 
-          {/* صفحة الفيديو */}
+          {/* صفحة الفيديو الاحترافية */}
           <Route path="/video/:id" element={<VideoPage />} />
 
           {/* صفحة 404 */}
