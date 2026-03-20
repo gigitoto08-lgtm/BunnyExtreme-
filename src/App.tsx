04067e1a385502c50e4daa3bea74e41b058978bf
Pro <gigitoto08@gmail.com>
2026-03-20 03:47:31 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,23 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 
-// صفحات الموقع
 import Index from "./pages/Index";
-import VideoPage from "./pages/video";
 import NotFound from "./pages/NotFound";
+import VideoPage from "./pages/video";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      {/* Toast notifications */}
       <Toaster />
       <Sonner />
-
-      {/* Routing */}
       <BrowserRouter>
         <Routes>
           {/* الصفحة الرئيسية */}
           <Route path="/" element={<Index />} />
 
-          {/* صفحة الفيديو الاحترافية */}
+          {/* صفحة الفيديو */}
           <Route path="/video/:id" element={<VideoPage />} />
 
           {/* صفحة 404 */}
